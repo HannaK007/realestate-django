dc992ebf131147c345e11f1845f2fd5f3e674e18
Hannak007 <hannak007@gmail.com>
2026-03-30 14:51:12 +0400

    cloudinary config edit

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 cloudinary.config(
     cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
     api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
 )
 
 CLOUDINARY_STORAGE = {
